ed390bd67669248229e81d4abcb60abd6de2f6ae
jefeRivas <95534532+jefeRivas@users.noreply.github.com>
2021-12-06 23:01:49 -0500

    06122021

--- src/App.js
+++ src/App.js
@@ -6,6 +6,33 @@ function App() {
    <div className="container mt-5">
      <h1>Tareas</h1>
      <hr/>
+     <div className="row">
+        <div className="col-8">
+          <h4 className= "text-center"> Lista de tareas</h4>
+          <ul className="list-group">
+              <li className ="list-group-item">
+                <span>Nombre de la tarea</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right">Editar</button>
+              </li>
+          </ul>
+        </div>
+        <div className="col-4">
+        <h4 className= "text-center"> Formulario</h4>
+        <form>
+          <input
+            type="text"
+            className="form-control mb-2"
+            placeholder="ingrese la  tarea"
+          />
+          <button 
+          className="btn btn-dark btn-block"
+          type="submit"
+          >
+            Agregar</button>
+        </form>
+        </div>
+     </div>
    </div>
   )
 }
